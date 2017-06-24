42bfc57667bf192e20b5900af99a7bfc8ba066e5
Adams LaBorde <alaborde@umbel.com>
2017-06-23 21:33:16 -0500

    add output file validation

---
+++ tests/test_output.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+
+class TestOutputFile(TestCase):
+
+    def setUp(self):
+        self.csv_file = open('./tests/test_input.csv', 'rU')
+        self.expected_arff_file = open('./tests/expected_output.arff')
+        self.actual_arff_file = None  # TODO: Implement this
+
+    def tearDown(self):
+        self.csv_file.close()
+        self.expected_arff_file.close()
+        self.actual_arff_file.close()
+
+    def test_arff_comments(self):
+        for actual_line in self.actual_arff_file:
+            expected_line = self.expected_arff_file.readline()
+            self.assertEqual(actual_line, expected_line)
+
+    def test_arff_header(self):
+        csv_header = self.csv_file.readline()
+
+        for line in self.actual_arff_file:
+            if line.startswith('@ATTRIBUTE'):
+                header_val = line.split(' ')[1]
+                csv_header.pop(csv_header.index(header_val))
+                self.assertIn(header_val, csv_header)
+
+            if line.startswith('@DATA'):
+                break
+
+        self.assertEqual(len(csv_header), 0)
+
+    def test_arff_data(self):
+        self.csv_file.next()
+        for csv_line in self.csv_file:
+            arff_line = self.actual_arff_file.readline()
+            self.assertEqual(csv_line, arff_line)
+
+
