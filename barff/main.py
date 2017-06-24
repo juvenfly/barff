4a2f74286e0651697914bb9191004f917e1783eb
Adams LaBorde <alaborde@umbel.com>
2017-06-23 22:05:42 -0500

    class it up

--- barff/main.py
+++ barff/main.py
@@ -7,88 +7,101 @@ from maps import PANDAS_TO_ARFF
 arff_comments = [['% This is a comment'], ['% This is also a comment'], ['%']]
 
 
-def main():
-    data_frame = pd.read_csv('./tests/test_input.csv')
-    output_file = open('./tmp/output.arff', 'w+')
-    writer = csv.writer(output_file, delimiter=',', quotechar='', escapechar='\\', quoting=csv.QUOTE_NONE)
+class ArffConverter(object):
+    def __init__(self):
+        self.data_frame = None
+        self.writer = None
 
-    for comment in arff_comments:
-        writer.writerow(comment)
+    def main(self):
+        data_frame = pd.read_csv('./tests/test_input.csv')
+        output_file = open('./tmp/output.arff', 'w+')
+        writer = csv.writer(output_file, delimiter=',', quotechar='', escapechar='\\', quoting=csv.QUOTE_NONE)
 
-    writer.writerow(['@RELATION "test_relation"'])
-    writer.writerow([])
+        self.collect_comments()
 
-    arff_header = convert_header(data_frame)
+        for comment in arff_comments:
+            writer.writerow(comment)
 
-    for line in arff_header:
-        writer.writerow(line)
+        writer.writerow(['@RELATION "test_relation"'])
+        writer.writerow([])
 
-    writer.writerow([])
-    writer.writerow(['@DATA'])
+        arff_header = self.convert_header(data_frame)
 
-    for row in arff_rows(data_frame):
-        writer.writerow(row)
+        for line in arff_header:
+            writer.writerow(line)
 
-    output_file.close()
+        writer.writerow([])
+        writer.writerow(['@DATA'])
 
+        for row in self.arff_rows(data_frame):
+            writer.writerow(row)
 
-def convert_header(data_frame):
-    """
-    Converts header from data_frame to arff
-    :param data_frame: pandas data_frame
-    :param output_file: arff output file
-    :return: list of lines
-    """
-    arff_header = []
+        output_file.close()
 
-    for column in data_frame.columns:
-        attribute_name = column
-        pd_dtype = str(data_frame[attribute_name].dtype)
+    def collect_comments(self):
+        line_number = 0
+        while True:
+            comment = raw_input("Please input comment line {} or 'X' to continue: ".format(line_number))
+            if comment.lower() in ['x', "'x'"]:
+                break
+            comment = ['% {}'.format(comment)]
+            print comment
 
-        arff_dtype = map_data_types(pd_dtype, data_frame, column)
+    def convert_header(self, data_frame):
+        """
+        Converts header from data_frame to arff
+        :param data_frame: pandas data_frame
+        :param output_file: arff output file
+        :return: list of lines
+        """
+        arff_header = []
 
-        line = ['@ATTRIBUTE {} {}'.format(attribute_name, arff_dtype)]
-        arff_header.append(line)
+        for column in data_frame.columns:
+            attribute_name = column
+            pd_dtype = str(data_frame[attribute_name].dtype)
 
-    print arff_header
-    return arff_header
+            arff_dtype = self.map_data_types(pd_dtype, data_frame, column)
 
+            line = ['@ATTRIBUTE {} {}'.format(attribute_name, arff_dtype)]
+            arff_header.append(line)
 
-def map_data_types(pd_dtype, data_frame, column):
-    """
-    Converts a pandas data type to the corresponding arff data type
-    :param pd_dtype: pandas data type as string
-    :return: arff data type as string
-    """
-    try:
-        arff_dtype = PANDAS_TO_ARFF[pd_dtype]
-    except KeyError:
-        if pd_dtype == 'bool':
-            # TODO: Implement this
-            arff_dtype = map_column_to_arff_class(data_frame, column)
-        else:
-            raise
+        print arff_header
+        return arff_header
 
-    return arff_dtype
+    def map_data_types(self, pd_dtype, data_frame, column):
+        """
+        Converts a pandas data type to the corresponding arff data type
+        :param pd_dtype: pandas data type as string
+        :return: arff data type as string
+        """
+        try:
+            arff_dtype = PANDAS_TO_ARFF[pd_dtype]
+        except KeyError:
+            if pd_dtype == 'bool':
+                # TODO: Implement this
+                arff_dtype = self.map_column_to_arff_class(data_frame, column)
+            else:
+                raise
 
+        return arff_dtype
 
-def map_column_to_arff_class(data_frame, column):
-    """
-    Converts 'bool' data type to arff format
-    :return: arff class format
-    """
-    return "NOT YET IMPLEMENTED"
+    def map_column_to_arff_class(self, data_frame, column):
+        """
+        Converts 'bool' data type to arff format
+        :return: arff class format
+        """
+        return "NOT YET IMPLEMENTED"
 
-
-def arff_rows(data_frame):
-    """
-    Generator that yields arff rows from pandas dataframe
-    :param data_frame: pandas dataframe
-    :return: arff row
-    """
-    for pd_row in data_frame.values:
-        row = [str(item) for item in pd_row if not isinstance(item, str) or item]
-        yield row
+    def arff_rows(self, data_frame):
+        """
+        Generator that yields arff rows from pandas dataframe
+        :param data_frame: pandas dataframe
+        :return: arff row
+        """
+        for pd_row in data_frame.values:
+            row = [str(item) for item in pd_row if not isinstance(item, str) or item]
+            yield row
 
 if __name__ == '__main__':
-    main()
+    converter = ArffConverter()
+    converter.main()
