8a63cf47b28e132023ea6f5de667792c966b3b87
Adams LaBorde <alaborde@umbel.com>
2017-06-23 21:46:03 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='barff',
+    version='0.1dev',
+    packages=['barff'],
+    license='GPL 3.0',
+    long_description=open('README.md').read(),
+)
