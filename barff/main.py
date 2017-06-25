e549626bb3972b82b67347f3f97193dbf169216f
Adams LaBorde <alaborde@umbel.com>
2017-06-25 15:35:34 -0500

    add docstrings

--- barff/main.py
+++ barff/main.py
@@ -104,6 +104,11 @@ class ArffConverter(object):
 
 
 def format_val(val):
+    """
+    Helper method that applies all formatting methods on a given val.
+    :param val: raw value as string
+    :return: formatted string
+    """
     result = val
     result = quote_if_space(result)
     result = replace_nans(result)
@@ -111,6 +116,11 @@ def format_val(val):
 
 
 def quote_if_space(val):
+    """
+    Adds double quotes around a string value if it contains a space.
+    :param val: raw value as string
+    :return: formatted string
+    """
     result = val
     if ' ' in val:
         result = '"' + val + '"'
@@ -118,6 +128,12 @@ def quote_if_space(val):
 
 
 def replace_nans(val):
+    """
+    Replaces nan values with single question mark
+    :param val: raw value as string
+    :return: formatted string
+    """
+    # TODO: Ideally this should happen earlier, before pandas NaN is stringified and .lower()ed for safety's sake.
     result = val
     if val == 'nan':
         result = '?'
