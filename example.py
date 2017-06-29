ee6762803a3366b13c60c7a82a4760afa4b08b55
Adams LaBorde <alaborde@umbel.com>
2017-06-29 18:10:22 -0500

    update example script

--- example.py
+++ example.py
@@ -1,18 +1,11 @@
-import numpy as np
+from barff.maps import CSV_TO_PANDAS
 
 import barff
 
 csv_file = './tests/test_input.csv'
 output_file = './tmp/output.arff'
 relation = 'test relation'
-field_map = {
-    'full name': str,
-    'gender': str,
-    'email': str,
-    'phone': str,
-    'is_cool': np.bool,
-    'birthday': str,
-}
+field_map = CSV_TO_PANDAS
 
 barff.csv_to_arff(csv_file, output_file, relation, field_map)
 barff.arff_to_csv(output_file, './tmp/csv_output.csv')
