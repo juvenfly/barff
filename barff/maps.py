e3497b1afce7a06209911159ef95b5a926476078
Adams LaBorde <alaborde@umbel.com>
2017-07-04 16:37:12 -0500

    update csv_to_pandas map

--- barff/maps.py
+++ barff/maps.py
@@ -17,7 +17,7 @@ CSV_TO_PANDAS = {
     },
     'gender': {
         'pandas_dtype': str,
-        'arff_dtype': 'STRING',
+        'arff_dtype': '{M,F}',
     },
     'email': {
         'pandas_dtype': str,
@@ -25,15 +25,15 @@ CSV_TO_PANDAS = {
     },
     'phone': {
         'pandas_dtype': str,
-        'arff_dtype': 'STRING',
+        'arff_dtype': 'NUMERIC',
     },
     'is_cool': {
         'pandas_dtype': np.bool,
-        'arff_dtype': 'CLASS',
+        'arff_dtype': '{TRUE,FALSE}',
     },
     'birthday': {
         'pandas_dtype': str,
-        'arff_dtype': 'STRING',
+        'arff_dtype': 'DATE "yyyy-MM-dd"',
     },
 }
 
